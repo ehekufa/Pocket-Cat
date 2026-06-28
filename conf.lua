ae6bfb164c56d5a7c9d0df3ce7acf7681df52b74
ehekufa <newcatroidfive5@gmail.com>
2026-06-28 09:53:09 +0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -3,15 +3,14 @@ function love.conf(t)
     t.version = "12.0"
     t.console = false
 
-    t.window.width = 1024          -- фиксированный размер окна
+    t.window.width = 1024
     t.window.height = 768
-    t.window.fullscreen = false    -- оконный режим по умолчанию
-    t.window.resizable = true      -- разрешаем менять размер
+    t.window.fullscreen = false
+    t.window.resizable = true
     t.window.minwidth = 800
     t.window.minheight = 600
 
-    -- на мобильных пусть будет fullscreen
-    if love.system.getOS() == "Android" or love.system.getOS() == "iOS" then
+    if love._os == "Android" or love._os == "iOS" then
         t.window.fullscreen = true
         t.window.resizable = false
     end
